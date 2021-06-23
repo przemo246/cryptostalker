4b31c38e227fd595dcb5b4a5058bd72ad63e404d
Przemek <przemo247@outlook.com>
2021-06-23 22:23:13 +0200

    Add firebase config file

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAs3-QhsLoqozB87oy9P0cVqi8t_puIp-g',
+  authDomain: 'cryptostalker-18727.firebaseapp.com',
+  projectId: 'cryptostalker-18727',
+  storageBucket: 'cryptostalker-18727.appspot.com',
+  messagingSenderId: '620198505951',
+  appId: '1:620198505951:web:ff6ab2f9ccfbd9156aa86a',
+  measurementId: 'G-W01MPN2N4V',
+};
