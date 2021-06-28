e78318481b55a13def3a2860b01c8db192910881
Przemek <przemo247@outlook.com>
2021-06-28 21:13:45 +0200

    Render Modal component when Login or Register button is clicked

--- src/components/Layout/NotLoggedUser.js
+++ src/components/Layout/NotLoggedUser.js
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+import Modal from '../modal/Modal';
+
 export const NotLoggedUser = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="not-logged">
-      <button className="btn btn-gray">Log in</button>
-      <button className="btn btn-green">Register</button>
-    </div>
+    <>
+      <div className="not-logged">
+        <button className="btn btn-gray" onClick={() => setIsOpen(true)}>
+          Log in
+        </button>
+        <button className="btn btn-green" onClick={() => setIsOpen(true)}>
+          Register
+        </button>
+      </div>
+      <Modal open={isOpen} onClose={() => setIsOpen(false)} />
+    </>
   );
 };
