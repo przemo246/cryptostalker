8ee5303f4816c6fc3c9898129fa8b534e59cdd07
Przemek <przemo247@outlook.com>
2021-07-04 13:42:02 +0200

    Fix conditional rendering of modals

--- src/components/modal/AddAssetModal.js
+++ src/components/modal/AddAssetModal.js
@@ -1,5 +1,9 @@
 const AddAssetModal = () => {
-  return <div></div>;
+  return (
+    <div style={{ backgroundColor: "#fff", width: "300px", height: "300px" }}>
+      Add new asset
+    </div>
+  );
 };
 
 export default AddAssetModal;
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,24 +2,23 @@ import LoginModal from "./LoginModal";
 import RegisterModal from "./RegisterModal";
 import AddAssetModal from "./AddAssetModal";
 import reactDom from "react-dom";
-import { useState } from "react";
 
 const Modal = ({ open, type, onClose }) => {
-  const [modalType, setModalType] = useState(null);
   if (!open) return null;
+  let modal;
   if (type === "login") {
-    setModalType(<LoginModal onClose={onClose} />);
+    modal = <LoginModal onClose={onClose} />;
   }
   if (type === "register") {
-    setModalType(<RegisterModal onClose={onClose} />);
+    modal = <RegisterModal onClose={onClose} />;
   }
   if (type === "add-asset") {
-    setModalType(<AddAssetModal onClose={onClose} />);
+    modal = <AddAssetModal onClose={onClose} />;
   }
   return reactDom.createPortal(
     <>
       <div className="overlay"></div>
-      {modalType}
+      {modal}
     </>,
     document.getElementById("modal-root")
   );
