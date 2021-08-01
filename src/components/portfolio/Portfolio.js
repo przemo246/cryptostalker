a68e94d764b6330bb119f014f80f3985706a7ee0
Przemek <przemo247@outlook.com>
2021-08-02 00:31:20 +0200

    Move calculateTotalHoldingsAndTotalValue and removeDuplicates functions to useAssets hook, fix displaying assets on UI, add calculate balance

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -3,25 +3,8 @@ import { useAssets } from "../../hooks/useAssets";
 import { useModal } from "../../hooks/useModal";
 import { ModalController } from "../modal/ModalController";
 import { AssetItem } from "./AssetItem";
+import { formatNumber } from "./utils";
 import Loader from "react-loader-spinner";
-
-const calculateTotalHoldingsAndTotalValue = (assets) => {
-  return assets.reduce((acc, curr) => {
-    if (acc.findIndex((el) => el.id === curr.id) !== -1) {
-      const index = acc.findIndex((el) => el.id === curr.id);
-      acc[index].totalValue += curr.price * curr.holdings;
-      acc[index].totalHoldings += curr.holdings;
-    } else {
-      const template = {
-        id: curr.id,
-        totalHoldings: curr.holdings,
-        totalValue: curr.price * curr.holdings,
-      };
-      acc.push(template);
-    }
-    return acc;
-  }, []);
-};
 
 const formatMarketData = (marketData) => {
   return marketData.reduce((acc, curr) => {
@@ -38,67 +21,59 @@ const formatMarketData = (marketData) => {
   }, []);
 };
 
-const removeDuplicates = (arr, prop) => {
-  return Array.from(new Set(arr.map((el) => el[prop])));
-};
-
 export const Portfolio = () => {
-  const assets = useAssets();
-  const [calcAssets, setCalcAssets] = useState([]);
-  const [marketData, setMarketData] = useState([]);
-  const [assetIDs, setAssetIDs] = useState([]);
+  const [assets, assetIds] = useAssets();
+  const [assetsAndMarketData, setAssetsAndMarketData] = useState([]);
   const [summary, setSummary] = useState({ balance: 0, change: 0, profit: 0 });
   const [isOpen, toggleIsOpen] = useModal();
   const [loader, setLoader] = useState(true);
 
   useEffect(() => {
-    if (assetIDs.length === 0) {
-      return;
-    }
-    return Promise.all(
-      assetIDs.map(async (name) => {
-        try {
-          const response = await fetch(
-            `https://api.coingecko.com/api/v3/coins/${name}?localization=false&tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=false`
-          );
-          const data = await response.json();
-          return data;
-        } catch (err) {
-          console.error(err);
-        }
-      })
-    )
-      .then((data) => {
-        const formattedData = formatMarketData(data);
-        setMarketData(formattedData);
-        setLoader(false);
-      })
-      .catch((err) => console.error(err.message));
-  }, [assetIDs]);
-
-  useEffect(() => {
-    const uniqueAssetNames = removeDuplicates(assets, "id");
-    setAssetIDs(uniqueAssetNames);
-    const totalHoldingsAndValue = calculateTotalHoldingsAndTotalValue(assets);
-    setCalcAssets(totalHoldingsAndValue);
-  }, [assets]);
-
-  useEffect(() => {
-    const updateSummary = async () => {
+    const getMarketDataAndJoinWithAssets = async () => {
       try {
-        const market = await marketData;
-        const balance = calcAssets.reduce((acc, curr) => {
-          const { currentPrice } = market.find((el) => el.id === curr.id);
-          acc += curr.totalHoldings * currentPrice;
-          return acc;
-        }, 0);
-        console.log(balance);
+        const unformattedMarketData = await Promise.all(
+          assetIds.map(async (name) => {
+            try {
+              const response = await fetch(
+                `https://api.coingecko.com/api/v3/coins/${name}?localization=false&tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=false`
+              );
+              return await response.json();
+            } catch (err) {
+              console.error(err.message);
+            }
+          })
+        );
+        const formattedMarketData = formatMarketData(unformattedMarketData);
+        const assetsAndFormattedMarketData = formattedMarketData.map(
+          (marketEl) => {
+            const findAsset = assets.find((asset) => asset.id === marketEl.id);
+            return {
+              ...marketEl,
+              totalHoldings: findAsset.totalHoldings,
+              totalValue: findAsset.totalValue,
+            };
+          }
+        );
+        setAssetsAndMarketData(assetsAndFormattedMarketData);
+        setLoader(false);
       } catch (err) {
-        console.error(err);
+        console.error(err.message);
       }
     };
-    updateSummary();
-  }, [calcAssets, marketData]);
+    getMarketDataAndJoinWithAssets();
+  }, [assets, assetIds]);
+
+  useEffect(() => {
+    const balance = assetsAndMarketData.reduce((acc, curr) => {
+      acc += curr.totalHoldings * curr.currentPrice;
+      return acc;
+    }, 0);
+    setSummary((prevSummary) => ({
+      ...prevSummary,
+      balance: formatNumber(balance),
+    }));
+  }, [assetsAndMarketData]);
+
   return (
     <>
       <div className="portfolio">
@@ -142,16 +117,8 @@ export const Portfolio = () => {
                   visible={loader}
                 />
               ) : (
-                calcAssets.map((el, i) => {
-                  const data = marketData.find((data) => data.id === el.id);
-                  return (
-                    <AssetItem
-                      key={i}
-                      index={i}
-                      data={data}
-                      totalHoldings={el.totalHoldings}
-                    />
-                  );
+                assetsAndMarketData.map((data, i) => {
+                  return <AssetItem key={i} index={i} data={data} />;
                 })
               )}
             </>
