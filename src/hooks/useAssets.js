a109d9df20857ec1e490d66789f1118b8aef4bc8
Przemek <przemo247@outlook.com>
2021-08-01 23:46:51 +0200

    Modify useAssets hook to return unique  asset Ids and calculated assets

--- src/hooks/useAssets.js
+++ src/hooks/useAssets.js
@@ -2,8 +2,27 @@ import { useEffect, useState } from "react";
 import { db } from "../firebase.config";
 import { useUser } from "./useUser";
 
+const calculateTotalHoldingsAndTotalValue = (assets) => {
+  return assets.reduce((acc, curr) => {
+    if (acc.findIndex((el) => el.id === curr.id) !== -1) {
+      const index = acc.findIndex((el) => el.id === curr.id);
+      acc[index].totalValue += curr.price * curr.holdings;
+      acc[index].totalHoldings += curr.holdings;
+    } else {
+      const template = {
+        id: curr.id,
+        totalHoldings: curr.holdings,
+        totalValue: curr.price * curr.holdings,
+      };
+      acc.push(template);
+    }
+    return acc;
+  }, []);
+};
+
 export const useAssets = () => {
   const [assets, setAssets] = useState([]);
+  const [assetIds, setAssetIds] = useState([]);
   const user = useUser();
   useEffect(() => {
     db.collection("assets")
@@ -12,8 +31,11 @@ export const useAssets = () => {
       .onSnapshot((data) => {
         const dataArr = [];
         data.forEach((el) => dataArr.push(el.data()));
-        setAssets(dataArr);
+        const calculatedAssets = calculateTotalHoldingsAndTotalValue(dataArr);
+        const uniqueAssetIds = Array.from(new Set(dataArr.map((el) => el.id)));
+        setAssets(calculatedAssets);
+        setAssetIds(uniqueAssetIds);
       });
   }, [user?.uid]);
-  return assets;
+  return [assets, assetIds];
 };
