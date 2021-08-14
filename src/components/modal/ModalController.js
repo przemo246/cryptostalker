583956bca3a565e3e8fe80a3ba6b20e243ef7b9b
Przemek <przemo247@outlook.com>
2021-08-14 14:26:08 +0200

    Add edit asset modal to ModalController

--- src/components/modal/ModalController.js
+++ src/components/modal/ModalController.js
@@ -1,10 +1,11 @@
+import reactDom from "react-dom";
+import { Modal } from "./Modal";
 import { LoginModal } from "./LoginModal";
 import { RegisterModal } from "./RegisterModal";
 import { AddAssetModal } from "./AddAssetModal";
-import { Modal } from "./Modal";
-import reactDom from "react-dom";
+import { EditAssetModal } from "./EditAssetModal";
 
-export const ModalController = ({ open, type, onClose }) => {
+export const ModalController = ({ open, type, onClose, id }) => {
   if (!open) return null;
   let modal;
   if (type === "login") {
@@ -15,6 +16,9 @@ export const ModalController = ({ open, type, onClose }) => {
   }
   if (type === "add-asset") {
     modal = <AddAssetModal />;
+  }
+  if (type === "edit-asset") {
+    modal = <EditAssetModal id={id} />;
   }
   return reactDom.createPortal(
     <>
