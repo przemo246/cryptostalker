556121b196759abf767eb9f8ab3d69d10b377f40
Przemek <przemo247@outlook.com>
2021-08-14 14:40:09 +0200

    Display Modal when user clicks on edit asset icon

--- src/components/portfolio/AssetItem.js
+++ src/components/portfolio/AssetItem.js
@@ -1,7 +1,10 @@
 import { formatNumber } from "./utils";
 import { BiEdit } from "react-icons/bi";
+import { useModal } from "../../hooks/useModal";
+import { ModalController } from "../modal/ModalController";
 
 export const AssetItem = (props) => {
+  const [isOpen, toggleIsOpen] = useModal();
   const {
     data: {
       id,
@@ -15,32 +18,42 @@ export const AssetItem = (props) => {
     index,
   } = props;
   return (
-    <li className="assets__item" id={id}>
-      <div>{index + 1}</div>
-      <div>
-        <img src={img} alt="" />
-        {name}
-      </div>
-      <div>{symbol.toUpperCase()}</div>
-      <div>{formatNumber(currentPrice)} USD</div>
-      <div>
-        {totalHoldings} {symbol.toUpperCase()}
-      </div>
-      <div>{formatNumber(currentPrice * totalHoldings)} USD</div>
-      <div
-        style={
-          priceChangePerc24h >= 0 ? { color: "#00AF54" } : { color: "#EE2E31" }
-        }
-      >
-        {priceChangePerc24h.toFixed(2)}%
-      </div>
-      <div>
-        <BiEdit
-          style={{ cursor: "pointer", fontSize: "1.6rem", color: "#333" }}
-          title="Edit"
-          id={id}
-        />
-      </div>
-    </li>
+    <>
+      <li className="assets__item" id={id}>
+        <div>{index + 1}</div>
+        <div>
+          <img src={img} alt="" />
+          {name}
+        </div>
+        <div>{symbol.toUpperCase()}</div>
+        <div>{formatNumber(currentPrice)} USD</div>
+        <div>
+          {totalHoldings} {symbol.toUpperCase()}
+        </div>
+        <div>{formatNumber(currentPrice * totalHoldings)} USD</div>
+        <div
+          style={
+            priceChangePerc24h >= 0
+              ? { color: "#00AF54" }
+              : { color: "#EE2E31" }
+          }
+        >
+          {priceChangePerc24h.toFixed(2)}%
+        </div>
+        <div>
+          <BiEdit
+            style={{ cursor: "pointer", fontSize: "1.6rem", color: "#333" }}
+            title="Edit"
+            onClick={toggleIsOpen}
+          />
+        </div>
+      </li>
+      <ModalController
+        open={isOpen}
+        type="edit-asset"
+        onClose={toggleIsOpen}
+        id={id}
+      />
+    </>
   );
 };
