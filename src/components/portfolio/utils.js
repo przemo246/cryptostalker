e2cc80e5311bb74e4407a05d189416031bc5b396
Przemek <przemo247@outlook.com>
2021-08-14 10:02:27 +0200

    Refactor formatNumber function

--- src/components/portfolio/utils.js
+++ src/components/portfolio/utils.js
@@ -1,11 +1,7 @@
 export const formatNumber = (num) => {
-  let formattedNum;
   if (Math.abs(num) < 1) {
-    formattedNum = Number(num.toFixed(6));
+    return Number(num.toFixed(6));
   } else {
-    formattedNum = new Intl.NumberFormat("en-US").format(
-      Number(num.toFixed(2))
-    );
+    return new Intl.NumberFormat("en-US").format(Number(num.toFixed(2)));
   }
-  return formattedNum;
 };
