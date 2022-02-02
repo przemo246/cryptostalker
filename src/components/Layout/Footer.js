576ad11e4074c3a9c0867c659490345ab3450ac3
Przemek <przemo247@outlook.com>
2022-02-02 20:45:59 +0100

    Add CoinGecko credit in footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,15 +3,23 @@ import linkedinIcon from "../../img/linkedin.svg";
 export const Footer = ({ propsClassName }) => {
   return (
     <footer className={propsClassName}>
-      <a
-        href="https://www.linkedin.com/in/przemyslaw-welenc/"
-        className="footer__link"
-      >
-        <img src={linkedinIcon} className="footer__icon" alt="LinkedIn" />
-      </a>
-      <a href="https://github.com/Przemo246" className="footer__link">
-        <img src={githubIcon} className="footer__icon" alt="Github" />
-      </a>
+      <div>
+        Price data by{" "}
+        <a className="footer__link" href="https://www.coingecko.com/">
+          CoinGecko
+        </a>
+      </div>
+      <div className="footer__icons">
+        <a
+          href="https://www.linkedin.com/in/przemyslaw-welenc/"
+          className="footer__link"
+        >
+          <img src={linkedinIcon} className="footer__icon" alt="LinkedIn" />
+        </a>
+        <a href="https://github.com/Przemo246" className="footer__link">
+          <img src={githubIcon} className="footer__icon" alt="Github" />
+        </a>
+      </div>
     </footer>
   );
 };
